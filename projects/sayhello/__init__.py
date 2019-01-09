6af08faf94f0b4fadd8a9036dfaf4ecb705aa356
venus <venus@venusworld.cn>
2019-01-09 21:15:20 +0800

    add project sayhello.

---
+++ projects/sayhello/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask('sayhello')
+app.config.from_pyfile('settings.py')
+
+# 模板环境对象
+# http://jinja.pocoo.org/docs/latest/api/#jinja2.Environment
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+db = SQLAlchemy(app)
+
+# 因为需要将各个模块和程序实例关联起来，
+# 所以需要在构造文件中导入这些模块。
+# 因为这些模块也需要从构造文件中导入程序实例，
+# 所以为了避免循环依赖，这些导入语句在构造文件的末尾定义。
+from sayhello import views, errors, commands
