183da577aa7b3331918e7366c99c370707194c0a
venus <venus@venusworld.cn>
2019-01-20 17:08:54 +0800

    feat: implment follow using Ajax.

--- projects/albumy/blueprints/ajax.py
+++ projects/albumy/blueprints/ajax.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
+from flask_login import current_user
 from albumy.models import User
+from albumy.decorators import permission_required, confirm_required
 
 ajax_bp = Blueprint('ajax', __name__)
 
@@ -7,3 +9,36 @@ ajax_bp = Blueprint('ajax', __name__)
 def get_profile(user_id):
     user = User.query.get_or_404(user_id)
     return render_template('main/profile_popup.html', user=user)
+
+@ajax_bp.route('/followers-count/<int:user_id>')
+def followers_count(user_id):
+    user = User.query.get_or_404(user_id)
+    count = user.followers.count() - 1   # 减去自己
+    return jsonify(count=count)
+
+@ajax_bp.route('/follow/<username>', methods=['POST'])
+def follow(username):
+    if not current_user.is_authenticated:
+        return jsonify(message='请先登录！'), 403
+    if not current_user.confirmed:
+        return jsonify(message='请先确认邮箱！'), 400
+    if not current_user.can('FOLLOW'):
+        return jsonify(message='没有权限！'), 403
+    user = User.query.filter_by(username=username).first_or_404()
+    if current_user.is_following(user):
+        return jsonify(message='已经关注了。'), 400
+    
+    current_user.follow(user)
+    return jsonify(message='关注成功。')
+
+@ajax_bp.route('/unfollow/<username>', methods=['POST'])
+def unfollow(username):
+    if not current_user.is_authenticated:
+        return jsonify(message='请先登录！'), 403
+
+    user = User.query.filter_by(username=username).first_or_404()
+    if not current_user.is_following(user):
+        return jsonify(message='还没有关注。'), 400
+    
+    current_user.unfollow(user)
+    return jsonify(message='取消关注成功。')
--- projects/albumy/static/js/script.js
+++ projects/albumy/static/js/script.js
@@ -1,4 +1,100 @@
 $(function() {
+    // 对于 POST 方法的 Ajax 需要设置 CSRF token 保护，
+    // 设置在 HTTP 头部
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrf_token);
+            }
+        }
+    });
+
+    function update_followers_count(id) {
+        var $el = $('#followers-count-' + id);
+        $.ajax({
+            type: 'GET',
+            url: $el.data('href'),
+            success: function (data) {
+                $el.text(data.count);
+            }
+        });
+    }
+
+    function follow(e) {
+        var $el = $(e.target);
+        var id = $el.data('id');
+
+        $.ajax({
+            type: 'POST',
+            url: $el.data('href'),
+            success: function(data) {
+                $el.prev().show();
+                $el.hide();
+                update_followers_count(id);
+                toast('关注成功。')
+            }
+        });
+    }
+
+    function unfollow(e) {
+        var $el = $(e.target);
+        var id = $el.data('id');
+
+        $.ajax({
+            type: 'POST',
+            url: $el.data('href'),
+            success: function(data) {
+                $el.next().show();
+                $el.hide();
+                update_followers_count(id);
+                toast('取消关注成功。')
+            }
+        });
+    }
+
+    var hoverTimer = null;
+    var flash = null;
+    
+    function toast(body, category) {
+        clearTimeout(flash);  // 清除未完成的计时
+        var $toast = $('#toast');
+        
+        if (category === 'error') {
+            $toast.css('background-color', 'red');   // 错误类型消息
+        } else {
+            $toast.css('background-color', '#333');  // 普通类型消息
+        }
+
+        $toast.text(body).fadeIn();  // 淡入
+        flash = setTimeout(function() {
+            $toast.fadeOut();   // 3 秒后淡出
+        }, 3000);
+    }
+
+    $(document).on('click', '.follow-btn', follow.bind(this));
+    $(document).on('click', '.unfollow-btn', unfollow.bind(this));
+    $(document).ajaxError(function(event, request, settings) {  // 同一处理 ajax 的 error 回调
+        if (request.responseJSON && request.responseJSON.hasOwnProperty('message')) {
+            message = request.responseJSON.message;
+        } else if (request.responseText) {
+            var IS_JSON = true;
+            try {
+                var data = JSON.parse(request.responseText);   // 作为 JSON 解析
+            } catch (err) {
+                IS_JSON = false;
+            }
+
+            if (IS_JSON && data !== undefined && data.hasOwnProperty('message')) {
+                message = JSON.parse(request.responseText).message;
+            } else {
+                message = DEFAULT_ERROR_MESSAGE;    // 使用默认错误消息
+            }
+        } else {
+            message = DEFAULT_ERROR_MESSAGE;    // 使用默认错误消息
+        }
+        toast(message, 'error')
+    })
+
     $('#confirm-delete').on('show.bs.modal', function(e) {
         // 修改 <form> 的 action 为 <button> 的 data-href
         $('.delete-form').attr('action', $(e.relatedTarget).data('href'));
@@ -24,18 +120,6 @@ $(function() {
         $('#tags').show();
     });
     
-    var hoverTimer = null;
-    var flash = null;
-    
-    function toast(body) {
-        clearTimeout(flash);  // 清除未完成的计时
-        var $toast = $('#toast');
-        $toast.text(body).fadeIn();  // 淡入
-        flash = setTimeout(function() {
-            $toast.fadeOut();   // 3 秒后淡出
-        }, 3000);
-    }
-    
     function show_profile_popover(e) {
         var $el = $(e.target);
     
@@ -58,9 +142,6 @@ $(function() {
                             $el.popover('hide');
                         }, 200);
                     });
-                },
-                error: function(error) {
-                    toast('服务器出错，请稍后再试。');
                 }
             });
         }, 500);
