c50fd4b82beb1f94cb19d1356498814abe59284e
venus <venus@venusworld.cn>
2019-01-20 16:12:39 +0800

    feat: implement follow logic.

--- projects/albumy/blueprints/user.py
+++ projects/albumy/blueprints/user.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, request, render_template, current_app
+from flask import Blueprint, request, render_template, current_app, flash, redirect, url_for
+from flask_login import login_required, current_user
 from albumy.models import User, Photo, Collect
+from albumy.decorators import confirm_required, permission_required
+from albumy.utils import redirect_back
 
 user_bp = Blueprint('user', __name__)
 
@@ -20,3 +23,47 @@ def show_collections(username):
     pagination = Collect.query.with_parent(user).order_by(Collect.timestamp.desc()).paginate(page, per_page)
     collects = pagination.items
     return render_template('user/collections.html', user=user, pagination=pagination, collects=collects)
+
+@user_bp.route('/follow/<username>', methods=['POST'])
+@login_required
+@confirm_required
+@permission_required('FOLLOW')
+def follow(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    if current_user.is_following(user):
+        flash('已经关注过了。', 'info')
+        return redirect(url_for('.index', username=username))
+    
+    current_user.follow(user)
+    flash('关注成功。', 'success')
+    return redirect_back()
+
+@user_bp.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    if not current_user.is_following(user):
+        flash('还没有关注。', 'info')
+        return redirect(url_for('.index', username=username))
+    
+    current_user.unfollow(user)
+    flash('取消关注成功。', 'success')
+    return redirect_back()
+
+@user_bp.route('/<username>/followers')
+def show_followers(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page', 1, type=int)
+    per_page = current_app.config['ALBUMY_COMMENT_PER_PAGE']
+    pagination = user.followers.paginate(page, per_page)
+    follows = pagination.items
+    return render_template('user/followers.html', user=user, pagination=pagination, follows=follows)
+
+@user_bp.route('/<username>/following')
+def show_following(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page', 1, type=int)
+    per_page = current_app.config['ALBUMY_COMMENT_PER_PAGE']
+    pagination = user.following.paginate(page, per_page)
+    follows = pagination.items
+    return render_template('user/following.html', user=user, pagination=pagination, follows=follows)
